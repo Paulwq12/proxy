13c828a54dcc5768b0526a15eb884d6ce7990e1c
Paulwq12 <gracetacha1@gmail.com>
2024-11-26 16:22:45 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,40 @@
 const http = require('http');
 const { createProxyServer } = require('http-proxy');
+const os = require('os');
+
+// Function to get the server's IP address
+function getServerIp() {
+    const networkInterfaces = os.networkInterfaces();
+    for (const interface in networkInterfaces) {
+        for (const address of networkInterfaces[interface]) {
+            if (address.family === 'IPv4' && !address.internal) {
+                return address.address; // Return the first external IPv4 address found
+            }
+        }
+    }
+    throw new Error('No external IPv4 address found');
+}
 
 // Create a proxy server
 const proxy = createProxyServer({
-  target: 'https://www.youtube.com',
-  changeOrigin: true,
-  secure: false // Set to true if you want to verify SSL certificates
+    target: 'https://www.youtube.com',
+    changeOrigin: true,
+    secure: false // Set to true if you want to verify SSL certificates
 });
 
 // Create an HTTP server that listens for incoming requests
 const server = http.createServer((req, res) => {
-  proxy.web(req, res, (err) => {
-    console.error('Proxy error:', err);
-    res.writeHead(500, { 'Content-Type': 'text/plain' });
-    res.end('Something went wrong with the proxy.');
-  });
+    proxy.web(req, res, (err) => {
+        console.error('Proxy error:', err);
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Something went wrong with the proxy.');
+    });
 });
 
-// Specify the IP address and port for the proxy server
+// Get the server's IP address and set up the server
 const proxyPort = 3000;
-const proxyHost = '0.0.0.0'; // Listen on all interfaces
+const proxyHost = getServerIp(); // Use the server's IP address
 
 server.listen(proxyPort, proxyHost, () => {
-  console.log(`Proxy server is running at http://${proxyHost}:${proxyPort} and forwarding to YouTube`);
+    console.log(`Proxy server is running at http://${proxyHost}:${proxyPort} and forwarding to YouTube`);
 });
